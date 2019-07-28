28b3ae2da161e96444bd8e309e66f8942a615f9f
filipe-nobrega <filipesouza.nobrega@gmail.com>
2019-07-28 15:29:08 -0300

    Subindo estudos

---
+++ regressão/regressao_arvore_plano_saude.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+base = pd.read_csv('plano_saude2.csv')
+
+X = base.iloc[:, 0:1].values
+y = base.iloc[:, 1].values
+
+from sklearn.preprocessing import StandardScaler
+scaler_x = StandardScaler()
+X = scaler_x.fit_transform(X)
+scaler_y = StandardScaler()
+y = scaler_y.fit_transform(y)
+
+
+
+import matplotlib.pyplot as plt
+plt.scatter(X, y)
+plt.plot(X, regressor.predict(X), color = 'red')
+plt.title('Regressão com redes neurais')
+plt.xlabel('Idade')
+plt.ylabel('Custo')
